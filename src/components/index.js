984b867253195b63cf446cb8e9dee5ee181eb39e
unknown <108541117022@student.unismuh.ac.id>
2025-12-11 14:10:11 +0800

    tugas praktikum 2

--- src/components/index.js
+++ src/components/index.js
@@ -9,3 +9,4 @@
 // export { default as Button } from './Button';
 // export { default as Card } from './Card';
 // export { default as Header } from './Header';
+export { default as Header } from './Header';
